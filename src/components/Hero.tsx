7f439e90a03ef67990e5d59a913d3210baa5733a
vipulgupta28 <vilulgupta2802@gmail.com>
2025-05-23 02:11:07 +0530

    push2

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,7 +1,30 @@
-const Hero = () =>{
-    return(
-        <>
-        </>
-    )
-}
+import { motion } from "framer-motion";
+
+const Hero = () => {
+    const letters = ['M','i','d','j','o','u','r','n','e','y'];
+
+    return (
+        <div className="min-h-screen flex justify-center mt-50 bg-black">
+            <h1 className="text-[250px] text-white font-extrabold tracking-tight flex ">
+                {letters.map((letter, index) => (
+                    <motion.span
+                        key={index}
+                        initial={{ y: 200, scale: 0.8, opacity: 0 }}
+                        animate={{ y: 0, scale: 1, opacity: 1 }}
+                        transition={{
+                            type: "spring",
+                            stiffness: 200,
+                            damping: 20,
+                            mass: 1,
+                            delay: index * 0.1
+                        }}
+                    >
+                        {letter}
+                    </motion.span>
+                ))}
+            </h1>
+        </div>
+    );
+};
+
 export default Hero;
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,22 @@
 const Navbar = () =>{
     return(
         <>
+        <div className="flex p-5 justify-between">
+
+            <div className="w-full text-4xl text-white ">
+            midjourney
+        </div>
+
+        <div className="flex gap-10  text-white">
+            <p>About</p>
+            <p>Showcase</p>
+            <p>Career</p>
+            <p>FAQ</p>
+            <p>Contact</p>
+        </div>
+
+        </div>
+        
         </>
     )
 }
