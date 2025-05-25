f686e7386c9cac5cc387b6c536d1ea3f9252cf50
vipulgupta28 <vilulgupta2802@gmail.com>
2025-05-25 20:08:09 +0530

    Push14

--- src/ui/scroll.tsx
+++ src/ui/scroll.tsx
@@ -26,8 +26,7 @@ export const GoogleGeminiEffect = ({
       </p>
       <p className="text-xs md:text-xl font-normal text-center text-neutral-400 mt-4 max-w-lg mx-auto">
         {description ||
-          `Scroll this component and see the bottom SVG come to life wow this
-        works!`}
+          `Now powered with new and enhanced o1 models`}
       </p>
       <div className="w-full h-[890px] -top-60 md:-top-40  flex items-center justify-center bg-red-transparent absolute ">
         <button className="font-bold bg-white rounded-full md:px-4 md:py-2 px-2 py-1 md:mt-24 mt-8 z-30 md:text-base text-black text-xs  w-fit mx-auto ">
