3585310ba205d82f3eb5dddfeb2f004e79d88640
vipulgupta28 <vilulgupta2802@gmail.com>
2025-05-25 23:03:50 +0530

    Push14

--- src/components/waves.tsx
+++ src/components/waves.tsx
@@ -4,8 +4,7 @@ import { WavyBackground } from "../ui/waves";
 export function WavyBackgroundDemo() {
   return (
     <WavyBackground className="max-w-4xl mx-auto pb-40">
-     
-     
+
     </WavyBackground>
   );
 }
